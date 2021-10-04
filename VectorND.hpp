ced147ad335ee96f292805c81a0641cade86d420
narussable <narussable@gmail.com>
2021-10-04 16:37:15 -0500

    Correccion de biblioteca

--- VectorND.hpp
+++ VectorND.hpp
@@ -237,7 +237,7 @@ ostream &operator<<(ostream &os, const VectorND &a)
     return os;
 }
 
-// ------- SPECIAL VECTORS POVRAY ---------------------
+// ------- SPECIAL VECTORS 4 POV ---------------------
 
 VectorND O{0, 0, 0};
 VectorND I{1, 0, 0};
