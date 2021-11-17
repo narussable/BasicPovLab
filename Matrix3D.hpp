7662ee168560dabab1dcfed7fcdfed2abc0028d9
narussable <narussable@gmail.com>
2021-11-17 08:39:50 -0600

    Before a crazy idea in operator * for Matrix3D

--- Matrix3D.hpp
+++ Matrix3D.hpp
@@ -29,7 +29,7 @@ class Matrix3D : public Matrix{
         // Constructor:
         // This specific constructor generates is a rotational
         // matrix used to rotate around a vector axis.
-        Matrix3D(VectorND u, double th, bool degree = 0) : Matrix(4,4){
+        Matrix3D(VectorND u, double th, bool degree = 1) : Matrix(4,4){
             u = u.normalize();
             th *=  degree ? M_PI/180 : 1;
             cerr << u << endl;
@@ -38,6 +38,38 @@ class Matrix3D : public Matrix{
             this->A[1][0] = u_mn(u,th,{1,0,2},1) ; this->A[1][1] = u_nn(u[1],th)        ; this->A[1][2] = u_mn(u,th,{1,2,0},-1);
             this->A[2][0] = u_mn(u,th,{2,0,1},-1); this->A[2][1] = u_mn(u,th,{2,1,0},1) ; this->A[2][2] = u_nn(u[2],th)        ;
         }
+
+        // Constructor:
+        // This is a traslation matrix
+        Matrix3D(VectorND u) : Matrix(4,4){
+            for(int index = 0; index<3; ++index)
+                this->A[index][3] = u[index];
+        }
 };
     
+VectorND operator * (const Matrix3D& matA, const VectorND& a){
+    VectorND aux {0,0,0,1};
+    VectorND out {0,0,0};
+    cerr << "M------>" << endl;
+    cerr << matA << endl << endl;
+    cerr << "a------>" << a << endl << endl;
+    for(int index=0; index<a.dim(); ++index)
+        aux[index] = a[index];
+    for(int i=0; i<matA.dimN(); ++i){
+        double sum = 0.0;    
+        for(int j=0; j<matA.dimN(); ++j){
+            sum += matA[i][j] * aux[j];
+            cerr << matA[i][j] << '*' << aux[j] << '=';
+            cerr << sum << endl;
+        }
+        cerr << sum;
+        cerr << endl;
+        cerr << endl;
+
+        if( i!=3 )
+            cerr << out[i];
+    }
+    return out;
+}
+
 #endif
