3fda343fa957231e9652244ca8f370aae3a77629
narussable <narussable@gmail.com>
2021-12-03 18:17:57 -0600

    Several changes to NewPov. Addition of modifiers and objects in the class. Consider the bool var were changed to global in order to cost less mem at creating instance

--- NewPov.hpp
+++ NewPov.hpp
@@ -3,6 +3,10 @@
 
 #include "bmp2matInt.hpp"
 
+
+// Notice: Texture_on: surf_type_ -> TRUE; Texture_off: surf_type_ -> FALSE
+bool rot_ = false, trans_ = false, surf_type_ = false, open_ = false;
+
 // Povray writer
 class Povray : public ofstream{
     protected:
@@ -13,7 +17,6 @@ class Povray : public ofstream{
 
         VectorND rotV, traV, cam;
         int  blur_S = 500;
-        bool rot_ = false, trans_ = false;
         const char* tab = "\t";
 
         // -----------------------
@@ -27,11 +30,11 @@ class Povray : public ofstream{
 
         // Gamma value (1-2.2)
         void assumedGamma(float gam_num)
-        {  *this << endl << "global_settings{ asummed_gamma " << gam_num << " }\n\n";  }
+        {  *this << endl << "global_settings{ assumed_gamma " << gam_num << " }\n\n";  }
 
         // Blur samples
         void blurSamples(void)
-        {  *this << endl << "blur_samples " << this->blur_S << endl;  }
+        {  *this << endl << tab << "blur_samples " << this->blur_S << endl;  }
 
         // Write down function
         void write(const char * mssg)
@@ -65,18 +68,84 @@ class Povray : public ofstream{
             *this << endl;
         }
 
-        // ------------------
-        // OBJECT CONSTRUCTOR
-        // ------------------
+        // -------------------------------------------------------------------
+        // ------------------------- OBJECTS ---------------------------------
+        // -------------------------------------------------------------------
+
+        // -------------
+        // SETUP OBJECTS
+        // -------------
         
         void camera(const VectorND& u, const VectorND& v){
             *this << "camera {" << endl
                   << tab << "location " << u << endl
-                  << tab << "look_at " << v << endl;
+                  << tab << "look_at " << v;
             this->blurSamples();
+            this->setGeoMod();
+            *this << "}" << endl;
+        }
+
+        void light(const VectorND& pos, const VectorND& rgb) {
+            *this << "light_source { " << endl
+                  << tab << pos << ',' << rgb;
+            this->setGeoMod();
+            *this << "}" << endl;
+        }
+
+        // -----------------
+        // GRAPHICAL OBJECTS
+        // -----------------
+
+        void sphere(const VectorND& pos, double r) {
+            *this << "sphere {" << endl
+                  << tab << pos << ',' << r;
+            this->setGeoMod();
+            *this << "}" << endl;
+        }
+
+        void cone(const VectorND& base, const VectorND& end, double r_bas, double r_end) {
+            *this << "cone { " << endl
+                  << tab 
+                  << base << ',' << r_bas << ',' 
+                  << end << ',' 
+                  << end << ',' << r_end;
+            (open_) ? *this << tab << "open" << endl : *this << "";
+            this->setGeoMod();
+            *this << "}" << endl;
+        }
+
+        void cylinder(const VectorND& base, const VectorND& end, double r) {
+            *this << "cylinder {" << endl
+                  << tab << base << ',' << endl
+                  << tab << end << ',' << endl
+                  << tab << r << endl;
+           (open_) ? *this << tab << "open" : *this << "";
+            this->setGeoMod();
+           *this << "}" << endl; 
+        }
+
+        // -------------------------------------------------------------------
+        // ------------------------- MODIFIERS -------------------------------
+        // -------------------------------------------------------------------
+
+        // ---------------------
+        // GEOMETRICAL MODIFIERS
+        // ---------------------
+
+        void setGeoMod(void) {
+            (rot_ || trans_) ? *this << endl : *this << "";
             (rot_) ? *this << tab << "rotate " << rotV << endl : *this << "";
             (trans_) ? *this << "translate " << traV << endl : *this << "";
-            *this << "}";
+            (rot_ || trans_) ? *this << endl : *this << "";
+        }
+
+        // -----------------
+        // SURFACE MODIFIERS
+        // -----------------
+
+        void setSurMod(void) {
+            const char * surface = (surf_type_) ? "texture { " : "pigment { ";
+             
         }
 };
 
